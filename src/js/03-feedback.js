89bcefe4d152f41bec470d655459ab0baec59d6c
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-07-06 00:12:50 +0300

    Update (remarks from the mentor)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,8 +3,8 @@ const refs = {
 };
 
 const throttle = require('lodash.throttle');
-refs.form.addEventListener('input', throttle(onInput, 500))
-refs.form.addEventListener('submit', throttle(onSubmit, 500))
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onSubmit);
 
 const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
 
@@ -13,13 +13,13 @@ function onInput(e) {
     formData[nameInput] = e.target.value;
 
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-}
+};
 
 // Return value from localStorage
 Object.entries(formData).map((el) => {
     for (let i = 0; i < refs.form.length; i += 1) {
         if (refs.form[i].name === el[0]) {
-            refs.form[i].value = el[1]
+            refs.form[i].value = el[1];
         };
     };
 });
@@ -29,6 +29,6 @@ function onSubmit(e) {
     console.log(formData);
     localStorage.clear();
     for (let i = 0; i < refs.form.length; i+=1) {
-        refs.form[i].value = ''
-    }
-}
+        refs.form[i].value = '';
+    };
+};
